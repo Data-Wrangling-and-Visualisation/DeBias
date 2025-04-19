367826bb5e1af7de9f5b9dfec994c4a89d7cd810
Anton Kudryavtsev <dartt0n@ya.ru>
2025-04-19 21:33:45 +0300

    feat: add wordstore to store results of nlp module

--- debias/core/__init__.py
+++ debias/core/__init__.py
@@ -1,4 +1,6 @@
 from . import configs as configs
 from . import metastore as metastore
 from . import models as models
+from . import parser as parser
 from . import s3 as s3
+from . import wordstore as wordstore
